f9a8286890bd1c315448b4768828bb0302cbc4da
Dariusz Okonski <darek200180@gmail.com>
2019-11-12 06:19:17 +0000

    add classes for styling and padToThree function and new pictures API

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 const Pokecard = ({ id, name, type, exp }) => {
-  let imgSrc = `${POKE_API}${id}.png`;
+  let imgSrc = `${POKE_API}${padToThree(id)}.png`;
 
   return (
     <div className="Pokecard">
-      <h1>{name}</h1>
+      <h1 className="Pokecard-title">{name}</h1>
       <img src={imgSrc} alt={name} />
-      <div>Type: {type}</div>
-      <div>EXP: {exp}</div>
+      <div className="Pokecard-data">Type: {type}</div>
+      <div className="Pokecard-data">EXP: {exp}</div>
     </div>
   );
 }
